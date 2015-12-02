8677cb6c3c7dadc779b94c6588a2083017c2cd1d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-12-02 20:39:31 +0900

    :construction: implement template finder

--- lib/ruboty/ragoon/template.rb
+++ lib/ruboty/ragoon/template.rb
@@ -2,8 +2,16 @@ module Ruboty
   module Ragoon
     module Template
       def render_template(template_name, variables = {})
-        template = ::Tilt.new(File.join(TEMPLATE_DIR, "#{template_name}.erb"))
+        template = ::Tilt.new(find_template(template_name))
         template.render(nil, variables)
+      end
+
+      def find_template(template_name)
+        [File.join(ENV['PWD'], 'templates'), TEMPLATE_DIR].each do |dirname|
+          template_file = Dir.glob(File.join(dirname, "#{template_name}*")).first
+          return template_file unless template_file.nil?
+        end
+        raise "Template Missing : #{template_name} is not found"
       end
     end
   end
