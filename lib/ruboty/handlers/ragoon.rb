e14520d8d077a345f6354fe41229a0ff18dd80a1
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-12-02 23:51:09 +0900

    :secret: check message is private or not

--- lib/ruboty/handlers/ragoon.rb
+++ lib/ruboty/handlers/ragoon.rb
@@ -6,10 +6,15 @@ module Ruboty
       def schedule(message)
         target_date = parse_date(message[:date])
         events = ::Ruboty::Ragoon::Event.new(target_date)
-        message.reply(events.render)
+        message.reply(events.render(private: check_private(message)))
       end
 
       private
+
+      def check_private(message)
+        owner = message.original[:robot].send(:adapter).send(:user_info, ENV['SLACK_OWNER_ID'])
+        message.from.start_with?('D') && owner['name'] == message.from_name
+      end
 
       def parse_date(date)
         case date.downcase
