bc9aa20a7cf7a638da28568fa7bda65ba31c2efb
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-12-02 14:47:16 +0900

    :construction: implmement handler

--- lib/ruboty/handlers/ragoon.rb
+++ lib/ruboty/handlers/ragoon.rb
@@ -1,6 +1,12 @@
 module Ruboty
   module Handlers
     class Ragoon < Base
+      on(/ragoon( me)? (?<date>.+)/, name: 'schedule', description: 'retrieve schedule from garoon')
+
+      def schedule(message)
+        target_date = Date.parse(message[:date]) rescue Date.today
+        message.reply(target_date.to_s)
+      end
     end
   end
 end
