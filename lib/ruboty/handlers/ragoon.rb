b2dcfd35205233e8051d2749062543b51be516a9
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-12-02 21:19:51 +0900

    :construction: implement option parser

--- lib/ruboty/handlers/ragoon.rb
+++ lib/ruboty/handlers/ragoon.rb
@@ -4,9 +4,26 @@ module Ruboty
       on(/ragoon( me)? (?<date>.+)/, name: 'schedule', description: 'retrieve schedule from garoon')
 
       def schedule(message)
-        target_date = Date.parse(message[:date]) rescue Date.today
+        target_date = parse_date(message[:date])
         events = ::Ruboty::Ragoon::Event.new(target_date)
         message.reply(events.render)
+      end
+
+      private
+
+      def parse_date(date)
+        case date.downcase
+        when 'today'
+          Date.today
+        when 'tomorrow'
+          Date.today + 1
+        when 'yesterday'
+          Date.today - 1
+        else
+          Date.parse(date)
+        end
+      rescue
+        Date.today
       end
     end
   end
